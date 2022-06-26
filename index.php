61907fa9c74cf4d50be0aa57cfa9d0c35a466cdb
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-06-26 12:29:20 +0200

    Clean up HTML & CSS and restructure

--- index.php
+++ index.php
@@ -3,24 +3,29 @@
 <html lang="pl-PL" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>Tramwajarz</title>
+    <title>SRG Console</title>
     <link href="http://fonts.cdnfonts.com/css/bahnschrift" rel="stylesheet">
     <link href="styles/styles.css" rel="stylesheet" />
 </head>
 <body>
-<div class="page">
-    <div id="panel">
+<div id="container">
+    <div class="form-container">
         <form action="panel.php" method="get">
-            <label id="line" for ="linia">Linia</label>
-            <input class="input" type="text" id="linia" name="linia"><br /><br />
-            <label for="brygada">Brygada</label>
-            <input class="input" type="text" id="brygada" name="brygada"><br /> <br />
-            <label id="typDnia" for="typ">Typ dnia</label>
-            <input class="input" type="text" id="typ" name="typ">
-            <input type="submit" value="Zatwierdź"  class="actual"><br />
+            <div class="column">
+                <label for="linia">Linia</label>
+                <label for="brygada">Brygada</label>
+                <label for="typ">Typ dnia</label>
+            </div>
+            <div class="column">
+                <input class="input" type="text" id="linia" name="linia">
+                <input class="input" type="text" id="brygada" name="brygada">
+                <input class="input" type="text" id="typ" name="typ">
+            </div>
+                
+                <!--<input type="submit" value="Zatwierdź" class="actual">-->
+            
         </form>
     </div>
 </div>
-
 </body>
 </html>
